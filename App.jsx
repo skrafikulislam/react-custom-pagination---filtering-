2a9dbb25269e367518368804462968530658e3f1
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2024-04-14 01:23:50 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import data from "./Data.json";
+import "./App.css";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordPerPage = 5;
+  const lastIndex = currentPage * recordPerPage;
+  const firstIndex = lastIndex - recordPerPage;
+  const records = data.slice(firstIndex, lastIndex);
+  const nPage = Math.ceil(data.length / recordPerPage);
+  const numbers = [...Array(nPage + 1).keys()].slice(1);
+
+  const previousHandle = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const nextHandle = () => {
+    if (currentPage !== nPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const changeHandle = (m) => {
+    setCurrentPage(m);
+  };
+  return (
+    <div className="app">
+      <div className="heading">
+        <h1>Blow is the Data</h1>
+      </div>
+      <div className="search">
+        <input
+          placeholder="Search Your Data Here"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="table">
+        <table className="table">
+          <thead>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </thead>
+          <tbody>
+            {records
+              .filter((item) =>
+                search.toLowerCase() === ""
+                  ? item
+                  : item.name.toLowerCase().includes(search) ||
+                    item.email.toLowerCase().includes(search)
+              )
+              .map((item, i) => (
+                <tr key={i}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>{" "}
+      </div>
+      <div className="navigate">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link click" onClick={previousHandle}>
+              Previous
+            </a>
+          </li>
+          {numbers.map((item, i) => (
+            <li
+              className={`page-item ${currentPage === item ? "active" : ""}`}
+              key={i}
+            >
+              <a className="page-link click" onClick={() => changeHandle(item)}>
+                {item}
+              </a>
+            </li>
+          ))}
+          <li className="page-item">
+            <a className="page-link click" onClick={nextHandle}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
